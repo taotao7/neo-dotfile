475e0e9f813a5c02432aa52b44f703b02888ad1b
taotao7 <moca_tao7@foxmail.com>
2025-12-11 18:19:06 +0800

    feat: update kitty and nvim configs

--- dot_config/nvim/lua/plugins/user.lua
+++ dot_config/nvim/lua/plugins/user.lua
@@ -212,9 +212,31 @@ return {
 	{
 		"sphamba/smear-cursor.nvim",
 		opts = {
-			stiffness = 0.5,
-			trailing_stiffness = 0.5,
-			matrix_pixel_threshold = 0.5,
+			-- Smear cursor when switching buffers or windows.
+			smear_between_buffers = true,
+
+			-- Smear cursor when moving within line or to neighbor lines.
+			-- Use `min_horizontal_distance_smear` and `min_vertical_distance_smear` for finer control
+			smear_between_neighbor_lines = true,
+
+			-- Draw the smear in buffer space instead of screen space when scrolling
+			scroll_buffer_space = true,
+
+			-- Set to `true` if your font supports legacy computing symbols (block unicode symbols).
+			-- Smears and particles will look a lot less blocky.
+			legacy_computing_symbols_support = false,
+
+			-- Smear cursor in insert mode.
+			-- See also `vertical_bar_cursor_insert_mode` and `distance_stop_animating_vertical_bar`.
+			smear_insert_mode = true,
+			vertical_bar_cursor_insert_mode = true,
+			max_length_insert_mode = 8,
+			stiffness_insert_mode = 0.55,
+			trailing_stiffness_insert_mode = 0.42,
+			damping_insert_mode = 0.92,
+			distance_stop_animating_vertical_bar = 0.2,
+			delay_event_to_smear = 0,
+			delay_after_key = 3,
 		},
 	},
 	{

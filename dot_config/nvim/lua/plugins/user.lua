16d6a3134b60c90262ff2b4ecd6adf8869c8a37e
taotao7 <moca_tao7@foxmail.com>
2025-12-26 11:41:10 +0800

    chore: update neovim plugins and add sidekick.nvim

--- dot_config/nvim/lua/plugins/user.lua
+++ dot_config/nvim/lua/plugins/user.lua
@@ -101,11 +101,11 @@ return {
 	--   vim.g.augment_workspace_folders = ["/Users/tao/workspace/org-management-temp"]
 	-- end,
 	-- },
-	{ "yetone/avante.nvim",     enabled = true },
+	{ "yetone/avante.nvim",     enabled = false },
 	{
 		"coder/claudecode.nvim",
 		dependencies = { "folke/snacks.nvim" },
-		enabled = true,
+		enabled = false,
 		config = true,
 		-- keys = {
 		-- 	{ "<leader>a",  nil,                              desc = "AI/Claude Code" },
@@ -242,5 +242,118 @@ return {
 	{
 		"tribela/vim-transparent",
 		enabled = false,
+	},
+	{
+		"folke/sidekick.nvim",
+		opts = {
+			-- add any options here
+			cli = {
+				mux = {
+					backend = "tmux",
+					enabled = true,
+				},
+			},
+		},
+		keys = {
+			{
+				"<tab>",
+				function()
+					-- if there is a next edit, jump to it, otherwise apply it if any
+					if require("sidekick").nes_jump_or_apply() then
+						return -- jumped or applied
+					end
+
+					-- if you are using Neovim's native inline completions
+					if vim.lsp.inline_completion.get() then
+						return
+					end
+
+					-- any other things (like snippets) you want to do on <tab> go here.
+
+					-- fall back to normal tab
+					return "<tab>"
+				end,
+				mode = { "i", "n" },
+				expr = true,
+				desc = "Goto/Apply Next Edit Suggestion",
+			},
+			{
+				"<c-.>",
+				function()
+					require("sidekick.cli").toggle()
+				end,
+				desc = "Sidekick Toggle",
+				mode = { "n", "t", "i", "x" },
+			},
+			{
+				"<leader>aa",
+				function()
+					require("sidekick.cli").toggle()
+				end,
+				desc = "Sidekick Toggle CLI",
+			},
+			{
+				"<leader>as",
+				function()
+					require("sidekick.cli").select()
+				end,
+				-- Or to select only installed tools:
+				-- require("sidekick.cli").select({ filter = { installed = true } })
+				desc = "Select CLI",
+			},
+			{
+				"<leader>ad",
+				function()
+					require("sidekick.cli").close()
+				end,
+				desc = "Detach a CLI Session",
+			},
+			{
+				"<leader>at",
+				function()
+					require("sidekick.cli").send({ msg = "{this}" })
+				end,
+				mode = { "x", "n" },
+				desc = "Send This",
+			},
+			{
+				"<leader>af",
+				function()
+					require("sidekick.cli").send({ msg = "{file}" })
+				end,
+				desc = "Send File",
+			},
+			{
+				"<leader>av",
+				function()
+					require("sidekick.cli").send({ msg = "{selection}" })
+				end,
+				mode = { "x" },
+				desc = "Send Visual Selection",
+			},
+			{
+				"<leader>ap",
+				function()
+					require("sidekick.cli").prompt()
+				end,
+				mode = { "n", "x" },
+				desc = "Sidekick Select Prompt",
+			},
+			-- Example of a keybinding to open Claude directly
+			{
+				"<leader>ac",
+				function()
+					require("sidekick.cli").toggle({ name = "claude", focus = true })
+				end,
+				desc = "Sidekick Toggle Claude",
+			},
+			{
+				"<leader>ao",
+				function()
+					require("sidekick.cli").toggle({ name = "opencode", focus = true })
+				end,
+				desc = "Sidekick Toggle opencode",
+			},
+		},
 	},
 }

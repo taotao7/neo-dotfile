189e8ebb925e518377682cbf32fd15f45286786f
taotao7 <moca_tao7@foxmail.com>
2026-01-22 10:32:10 +0800

    feat: add astrocommunity.pack.cpp import

--- dot_config/nvim/lua/community.lua
+++ dot_config/nvim/lua/community.lua
@@ -28,6 +28,7 @@ return {
 	{ import = "astrocommunity.pack.json" },
 	{ import = "astrocommunity.pack.html-css" },
 	{ import = "astrocommunity.pack.rust" },
+	{ import = "astrocommunity.pack.cpp" },
 	{ import = "astrocommunity.pack.typescript" },
 	{ import = "astrocommunity.pack.vue" },
 	{ import = "astrocommunity.pack.tailwindcss" },
